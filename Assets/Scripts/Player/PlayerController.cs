a11f993bba36e9fd16f52b967cd8c92d05e1bcbd
TheCindayra <eivind.johnsen@protonmail.com>
2022-03-21 09:46:00 +0100

    Created voiceline controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(FloatCapsule))]
 [RequireComponent(typeof(Movement))]
 [RequireComponent(typeof(Input))]
+[RequireComponent(typeof(VoiceLineController))]
+[RequireComponent(typeof(AudioSource))]
+
 public class PlayerController : MonoBehaviour
 {
     private Input m_Input;
     private Movement m_Movement;
     private PlayerCamera m_Camera;
     private FloatCapsule m_FloatCapsule;
+    private VoiceLineController _voiceLineController;
+    private AudioSource _audioSource;
+
+    private bool _hasCrossedMiddle;
 
     private void Awake()
     {
@@ -16,6 +24,8 @@ public class PlayerController : MonoBehaviour
         m_Camera = GetComponent<PlayerCamera>();
         m_Movement = GetComponent<Movement>();
         m_Input = GetComponent<Input>();
+        _voiceLineController = GetComponent<VoiceLineController>();
+        _audioSource = GetComponent<AudioSource>();
     }
     private void Start()
     {
@@ -27,7 +37,20 @@ public class PlayerController : MonoBehaviour
         m_Camera.Look(m_Input.lookVector);
         m_FloatCapsule.Jump(m_Input.jumpTrigger);
     }
-    private void FixedUpdate() {    
+    private void FixedUpdate() {
         m_Movement.Move(m_Input.moveDirection, m_Input.run);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HalfwayPoint"))
+        {
+            _hasCrossedMiddle = true;
+        }
+        else if (other.CompareTag("StoryPoint"))
+        {
+            _hasCrossedMiddle = false;
+            _voiceLineController.PlayNextVoiceLine(_audioSource);
+        }
     }
 }
