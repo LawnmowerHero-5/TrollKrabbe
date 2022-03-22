d0eb0495ffed913e0d70e9921e037175e46d9dd4
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2022-03-22 11:02:59 +0100

    Fixed voicelinecontrollerand listener again

--- Assets/Scripts/Audio/LoopListener.cs
+++ Assets/Scripts/Audio/LoopListener.cs
@@ -4,20 +4,35 @@ public class LoopListener : MonoBehaviour
 {
     private VoiceLineController m_VlController;
     public AudioSource audioSource;
+    private int m_SkippedLines = 0;
     private void Awake()
     {
         m_VlController = GetComponent<VoiceLineController>();
     }
     public void ReceiveLoopInfo(int loopCount)
     {
-        if (loopCount == 1 )
+        if (loopCount == 0)
         {
+            m_VlController.voiceLineIndex = 0;
+            m_SkippedLines = 0;
+        }
+        if (loopCount == 2|loopCount == 5)//etc all lines that are to be skipped, also enable shortcuts etc here
+        {
+            SkipNextLine();
             
+        }
+        if (m_VlController.voiceLineIndex == loopCount-m_SkippedLines)
+        {
+            StartNextLine();
         }
     }
     private void StartNextLine()
     {
         m_VlController.PlayNextVoiceLine(audioSource);
+    }
+    private void SkipNextLine()
+    {
+        m_SkippedLines++;
     }
     
 }
