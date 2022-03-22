b8e59065ef61d16d62d68720b2618aa480786a86
TheCindayra <eivind.johnsen@protonmail.com>
2022-03-22 23:05:52 +0100

    Fixed player prefab

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -13,30 +13,25 @@ namespace Audio
 
         public void PlayWalkAudio(Vector3 moveSpeed, AudioSource audioSource, Transform playerPosition)
         {
-            if (Mathf.Abs(moveSpeed.x) <= 1f || Mathf.Abs(moveSpeed.z) <= 1f) return;
             if (Time.time < audioTimer) return;
+            if (Mathf.Abs(moveSpeed.x) <= 0.1f || Mathf.Abs(moveSpeed.z) <= 0.1f) return;
 
             audioTimer = Time.time + audioFrequency;
 
             RaycastHit raycast;
             Physics.Raycast(playerPosition.position, Vector3.down, out raycast);
 
-            Debug.DrawRay(playerPosition.position, Vector3.down, Color.red);
-
             if (raycast.collider.CompareTag("Ground"))
             {
                 groundWalk.PlayRandomAudio(audioSource);
-                // audioTimer = Time.time + groundWalk.clipLength;
             }
             else if (raycast.collider.CompareTag("Water"))
             {
                 waterWalk.PlayRandomAudio(audioSource);
-                // audioTimer = Time.time + waterWalk.clipLength;
             }
             else if (raycast.collider.CompareTag("Rock"))
             {
                 rockWalk.PlayRandomAudio(audioSource);
-                // audioTimer = Time.time + rockWalk.clipLength;
             }
         }
     }
