5c5d5d19560a9a9e5995e1a0229e411940acfe5a
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2022-03-22 14:42:19 +0100

    Imported Skies, again

--- Assets/Scripts/LoopCounter/ExampleReceiver.cs
+++ Assets/Scripts/LoopCounter/ExampleReceiver.cs
@@ -12,12 +12,12 @@ namespace LoopCounter
             {
                 //Checks if the loop just finished, Example, just met the fox
                 case > 0:
-                    print("Loop " + loopCount + " Finished");
+                    // print("Loop " + loopCount + " Finished");
                     break;
                 //Checks if its in the middle of the loop, it's essentially loopCount.5
                 //This is only necessary if you want to have actions if you want anything to happen at the halfwayPoint
                 case < 0:
-                    print("Midway on Loop Nr." + -loopCount);
+                    // print("Midway on Loop Nr." + -loopCount);
                     break;
             }
         }
